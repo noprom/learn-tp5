1b12be181f99f086041abd7594ee43108aa34426
noprom <tyee.noprom@qq.com>
2016-07-04 15:36:56 +0800

    router config

--- application/route.php
+++ application/route.php
@@ -17,5 +17,12 @@ return [
         ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
         ':name' => ['index/hello', ['method' => 'post']],
     ],
+    // 路由参数name为可选
+    'hello/[:name]$' => 'index/hello',
+    // 当路由规则以$结尾的时候就表示当前路由规则需要完整匹配。
+ 	// http://tp5.com/hello // 正确匹配
+	// http://tp5.com/hello/thinkphp // 正确匹配
+	// http://tp5.com/hello/thinkphp/val/value // 不会匹配
 
+	
 ];
